6232f1e1af3803c176316863761e3e88947ce364
lisi <jiangwei@1000phone.com>
2018-11-23 10:36:29 +0800

    修改登录验证的逻辑

--- day07/02-示例代码/myChat/controllers/login.js
+++ day07/02-示例代码/myChat/controllers/login.js
@@ -1,30 +1,38 @@
 var userDao = require("../dao/userDao.js");
 var utils = require('utility');
 var config = require('../config.js');
-module.exports.doLogin = function (req, res,next) {
+module.exports.doLogin = function (req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
-  
+
   if (!username || !password) {
-    return res.send({err:'用户名或者密码不能为空'});
+    return res.send({
+      err: '用户名或者密码不能为空'
+    });
   }
-  
-  req.body.password = utils.md5(password+ config.md5_secret);
-  userDao.getUsers(req.body,function (err,data) {
+
+  req.body.password = utils.md5(password + config.md5_secret);
+  userDao.getUsers(req.body, function (err, data) {
     if (err) {
-      return res.send({err:'查询出错'});
+      return res.send({
+        err: '查询出错'
+      });
     }
-    if(data) {
+
+    // 如果没有查询到数据，查询的结果长度为0
+    if (data.length == 0) {
+      res.send({
+        err: '用户名或者密码错误'
+      });
+    } else {
       req.session.user = username;
       res.redirect('/home');
-    }else {
-      res.send({err:'用户名或者密码错误'});
     }
   })
 }
-module.exports.showRegister = function (req,res) {
+module.exports.showRegister = function (req, res) {
   res.render('register');
 }
-module.exports.showLogin = function (req,res) {
+module.exports.showLogin = function (req, res) {
   res.render('login');
 }
